8a4ba6ca05216e88123100b34a64366d70462d3e
goncalocesar01 <113987063+goncalocesar01@users.noreply.github.com>
2023-06-05 17:21:18 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -8,7 +8,7 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to the port---------------------------------------------------------------------------------------
-    server_address = ('localhost', 10000)
+    server_address = ('192.168.0.11', 10000)
     print('starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
 
@@ -18,29 +18,30 @@ def main():
     while True:
         # Wait for axis connection
         connection, client_address = sock.accept()
-        while True:
+        while client_address != "":
             # Receber o tamanho dos dados
             data_length = int(connection.recv(1024).decode())
-# Receber os dados completos
+            # Receber os dados completos
             data = connection.recv(data_length).decode()       
-                data = connection.recv(5000)
-                data = json.loads(data.decode())
-                JLX=data.get("JLX")
-                JLY=data.get("JLX")
-                JRX=data.get("JLX")
-                JZ=data.get("JLX")
-                A=data.get("JLX")
-                time.sleep(0.0001)
-                #print(JLX,JLY,JRX,JZ,A)
-                commands=f"{JLX},{JLY},{JRX},{JZ},{A}\n"
-                serial2.write(commands.encode())
+            data = connection.recv(5000)
+            data = json.loads(data.decode())
+            JLX=data.get("JLX")
+            JLY=data.get("JLX")
+            JRX=data.get("JLX")
+            JZ=data.get("JLX")
+            A=data.get("JLX")
+            time.sleep(0.0001)
+            #print(JLX,JLY,JRX,JZ,A)
+            commands=f"{JLX},{JLY},{JRX},{JZ},{A}\n"
+            serial2.write(commands.encode())
             
-                if serial2.in_waiting > 0:
-                # Ler a mensagem da porta serial
-                    Altura = serial2.readline().decode().rstrip()
-                # Exibir a mensagem
-                    sock.send(Altura.encode())
-    
+            if serial2.in_waiting > 0:
+            # Ler a mensagem da porta serial
+                Altura = serial2.readline().decode().rstrip()
+            # Exibir a mensagem
+                sock.send(Altura.encode())
+            if not connection.fileno():
+                break
         
 if __name__ == "__main__":
     main()
