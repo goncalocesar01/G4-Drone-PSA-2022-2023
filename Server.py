59a7ef1a15b1a32a6dcc90d348ad9603c2d01fb7
goncalocesar01 <113987063+goncalocesar01@users.noreply.github.com>
2023-06-10 18:57:19 +0100

    Add files via upload

--- Server.py
+++ Server.py
@@ -8,40 +8,43 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to the port---------------------------------------------------------------------------------------
-    server_address = ('192.168.0.11', 10000)
+    server_address = ('192.168.1.9', 10000)
     print('starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
 
     # Listen for incoming connections-----------------------------------------------------------------------------------
     sock.listen(1)
     serial2=serial.Serial('COM4',115200)
+    connection, client_address = sock.accept()
     while True:
         # Wait for axis connection
-        connection, client_address = sock.accept()
-        while client_address != "":
-            # Receber o tamanho dos dados
-            data_length = int(connection.recv(1024).decode())
-            # Receber os dados completos
-            data = connection.recv(data_length).decode()       
-            data = connection.recv(5000)
-            data = json.loads(data.decode())
-            JLX=data.get("JLX")
-            JLY=data.get("JLX")
-            JRX=data.get("JLX")
-            JZ=data.get("JLX")
-            A=data.get("JLX")
-            time.sleep(0.0001)
-            #print(JLX,JLY,JRX,JZ,A)
-            commands=f"{JLX},{JLY},{JRX},{JZ},{A}\n"
-            serial2.write(commands.encode())
-            
-            if serial2.in_waiting > 0:
-            # Ler a mensagem da porta serial
-                Altura = serial2.readline().decode().rstrip()
-            # Exibir a mensagem
-                sock.send(Altura.encode())
-            if not connection.fileno():
+        
+        
+        data= connection.recv(5000).decode()
+        if not connection.fileno():
                 break
+        if not data:
+            break
+            # Receber os dados completosunsupported operand type(s) for +=: 'dict' and 'str'
+        jsondata = json.loads(data)
+        JLX=jsondata["JLX"]
+        JLY=jsondata["JLY"]
+        JRX=jsondata["JRX"]
+        JZ=jsondata["JZ"]
+        A=jsondata["A"]
+       
+        print(JLX,JLY,JRX,JZ,A)
+        commands=f"{JLX},{JLY},{JRX},{JZ},{A}\n"
+        serial2.write(commands.encode())
+        time.sleep(0.1)    
+        if serial2.in_waiting > 0:
+            # Ler a mensagem da porta serial
+            Altura = serial2.readline().decode().rstrip()
+        
+               # Exibir a mensagem
+        connection.send(Altura.encode())
+
+    connection.close()
         
 if __name__ == "__main__":
     main()
